7c46f49b1b447095e4f2f9e508636de54fd5f778
Sasha <q.dev.null@gmail.com>
2021-02-10 03:30:48 +0100

    Replace the metrics module

--- model/metrics.py
+++ model/metrics.py
@@ -1,13 +1,9 @@
 from model.data import to_inference, pad
-from irmetrics.topk import recall as irecall
-
-
-def recall(y_true, y_pred=None, k=None, pad_token="<pad>"):
-    return irecall(y_true, y_pred, k=k, pad_token=pad_token)
+from irmetrics.topk import recall
 
 
 def recall_score(model, data):
     # Approximate the recall on the data set
     observed, gold = to_inference(data)
     preds = model.predict(observed)
-    return recall(pad(gold), preds).mean()
+    return recall(pad(gold), preds, pad_token=None).mean()
