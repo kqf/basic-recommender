16a5454c989975b447ab6028c137266a1d9ea2e1
kqf <q.dev.null@gmail.com>
2021-02-10 03:40:20 +0100

    Replace the custom metrics (#5)
    * Add the requirements
    
    * Fix the imports
    
    * Fix the naming for pad tokens
    
    * Replace the metrics module

--- model/metrics.py
+++ model/metrics.py
@@ -1,16 +1,9 @@
-import numpy as np
 from model.data import to_inference, pad
-
-
-def recall(y_true, y_pred=None, k=None, padding_label="<pad>"):
-    y_true, y_pred = np.atleast_2d(y_true, y_pred)
-    tp = (y_pred[:, :, None] == y_true[:, None]).any(axis=-1)
-    true = y_true != padding_label
-    return tp.sum(-1) / true.sum(-1)
+from irmetrics.topk import recall
 
 
 def recall_score(model, data):
     # Approximate the recall on the data set
     observed, gold = to_inference(data)
     preds = model.predict(observed)
-    return recall(pad(gold), preds).mean()
+    return recall(pad(gold), preds, pad_token=None).mean()
--- tests/test_models.py
+++ tests/test_models.py
@@ -5,8 +5,8 @@ from model.coo import build_model as build_coo
 from model.logistic import build_model as build_logistic
 from model.semantic import build_model as build_semantic
 
-from model.data import to_inference, pad
-from model.metrics import recall
+from model.data import to_inference
+from model.metrics import recall_score
 
 
 @pytest.mark.parametrize("build", [
@@ -19,5 +19,5 @@ def test_models(build, data):
     test, gold = to_inference(data)
     model = build()
     model.fit(test)
-    preds = model.predict(test)
-    recall(pad(gold), preds).mean()
+    model.predict(test)
+    print(recall_score(model, data))
